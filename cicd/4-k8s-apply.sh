4579d9fe7c5f4611442266a2376f4d4adb23f6d4
Juan C Jimenez <juancarlo14071@gmail.com>
2024-12-31 12:22:18 -0300

    fix: update Kubernetes deployment script to target the correct namespace

--- cicd/4-k8s-apply.sh
+++ cicd/4-k8s-apply.sh
@@ -6,8 +6,8 @@ load_config
 # Desplegar en Kubernetes
 echo "Iniciando despliegue en Kubernetes..."
 microk8s kubectl apply -k ../kustomization/$TARGET_ENVIROMENT || handle_error "despliegue 1"
-microk8s kubectl set image deployment/$APP_NAME $APP_NAME=$IMAGE -n service-management || handle_error "despliegue 2"
-microk8s kubectl rollout restart deployment/$APP_NAME -n service-management || handle_error "despliegue 3"
+microk8s kubectl set image deployment/$APP_NAME $APP_NAME=$IMAGE -n cryptofollow-dev || handle_error "despliegue 2"
+microk8s kubectl rollout restart deployment/$APP_NAME -n cryptofollow-dev || handle_error "despliegue 3"
 echo "Despliegue en Kubernetes completado con éxito."
 
 echo "Todas las acciones se completaron con éxito."
