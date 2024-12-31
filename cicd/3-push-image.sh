92a629a6f3533251e257d7d3d85600485c4137f2
Juan C Jimenez <juancarlo14071@gmail.com>
2024-12-31 11:14:35 -0300

    fix: update variable names for consistency in CI/CD scripts

--- cicd/3-push-image.sh
+++ cicd/3-push-image.sh
@@ -5,5 +5,5 @@ load_config
 
 # Empujar la imagen de Docker
 echo "Iniciando empuje de la imagen de Docker al registro..."
-docker push "$image:latest" || handle_error "empuje"
+docker push "$IMAGE:latest" || handle_error "empuje"
 echo "Empuje de la imagen de Docker completado con éxito."
--- cicd/4-k8s-apply.sh
+++ cicd/4-k8s-apply.sh
@@ -6,8 +6,8 @@ load_config
 # Desplegar en Kubernetes
 echo "Iniciando despliegue en Kubernetes..."
 microk8s kubectl apply -k ../k8s || handle_error "despliegue 1"
-microk8s kubectl set image deployment/$appName $appName=$image -n service-management || handle_error "despliegue 2"
-microk8s kubectl rollout restart deployment/$appName -n service-management || handle_error "despliegue 3"
+microk8s kubectl set image deployment/$APP_NAME $APP_NAME=$IMAGE -n service-management || handle_error "despliegue 2"
+microk8s kubectl rollout restart deployment/$APP_NAME -n service-management || handle_error "despliegue 3"
 echo "Despliegue en Kubernetes completado con éxito."
 
 echo "Todas las acciones se completaron con éxito."
