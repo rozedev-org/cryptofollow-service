009aa29d5a10a841c8301a77f881e218e8aeb0fb
Juan C Jimenez <juancarlo14071@gmail.com>
2024-12-31 11:18:04 -0300

    fix: specify Dockerfile path in build script to ensure correct image construction

--- cicd/2-build-image.sh
+++ cicd/2-build-image.sh
@@ -4,6 +4,6 @@ source ./utils.sh
 load_config
 
 echo "Iniciando construcción de la imagen de Docker..."
-docker build -t "$IMAGE" ../ || handle_error "construcción"
+docker build -t "$IMAGE" ../Dockerfile || handle_error "construcción"
 echo "Construcción de la imagen de Docker completada con éxito."
 
