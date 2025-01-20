614767886bfe02c8d3b4a94d86c5d3a30daecd78
Juan C Jimenez <juancarlo14071@gmail.com>
2025-01-20 16:14:58 -0300

    feat: add JWT authentication guard to wallet controller

--- src/wallet/controllers/wallet.controller.ts
+++ src/wallet/controllers/wallet.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { WalletService } from '../services/wallet.service';
 import {
   BalanceByCurrencyEntity,
   BalanceEntity,
 } from '../entities/wallet.entity';
+import { JwtAuthGuard } from '@app/auth/guards/jwt-authentication.guard';
 
 @ApiTags('wallet')
+@UseGuards(JwtAuthGuard)
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
